34243a1da6e67aacaa5dd76b57d69c1e6dfb0170
Satyam Shivam <kumarshivam71552@gmail.com>
2024-10-05 19:44:18 +0000

    add to Readme

---
+++ tut2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+ {
+    //   ---------> Addition of 2 number and taking input from user <----------
+    
+     int a,b,c;
+     cout<<"the value of a:\n";
+     cin>>a;
+     cout<<"the value of b:\n";
+     cin>>b;
+     c= a + b;
+     cout<<"the sum is:\n"<<c;
+     return 0;
+
+ }
+ 
