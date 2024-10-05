b2a56f8f0a2e9b80b880b7db2a32cd4e2dea93a0
Satyam Shivam <kumarshivam71552@gmail.com>
2024-10-05 19:22:28 +0000

    new code

---
+++ tut1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+    int main(){
+        int a = 9,b = 8; 
+        float pi = 3.14;
+        char  c = 'd';
+        bool e = false;
+        cout<<"\nthis is tutorial 1:";
+        cout<<"\nthe value of a:"<<a;
+        cout<<"\nthe value of b:"<<b;
+        cout<<"\nthe value of pi:"<<pi;
+        cout<<"\nthe value is e:"<<e;
+        cout<<"\nthe value is c:"<<c; 
+
+        return 0;
+    }
