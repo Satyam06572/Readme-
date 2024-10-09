af66da7c42f3eb7d73bc8aff8e52699121cc1ffd
Satyam Shivam <kumarshivam71552@gmail.com>
2024-10-09 11:20:37 +0000

    add to Readme

---
+++ tut8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  // pointer array
+
+  int i = 0;
+  int a = 0;
+
+  int age[] = {2, 4, 6, 7, 9};
+
+  cout << age[0] << endl;
+  cout << age[1] << endl;
+  cout << age[2] << endl;
+  cout << age[3] << endl;
+  cout << age[4] << endl;
+  return 0;
+}
