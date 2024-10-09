c2d55f8a7c79587438e7c6894b3b7f737de0e374
Satyam Shivam <kumarshivam71552@gmail.com>
2024-10-09 11:43:19 +0000

    add to Readme

---
+++ tut11.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+    
+    //Function
+
+void myfunction()
+{
+    cout<<"Hello everyone!"<<endl;
+    cout<<"My name is Shivam!"<<endl;
+    cout<<"I am frome bihar!"<<endl; 
+
+
+    
+    cout<<" the sum  of 'a' and 'b' is:"<<endl;
+    int a,b;
+    cout<<"enter the valu of a:"<<endl;
+    cin>>a;
+    cout<<"enter the value of b:"<<endl;
+    cin>>b;
+    cout<<"the sum is:"<<a+b<<endl;
+
+}
+
+int main()
+{
+    myfunction();
+    return 0;
+}
