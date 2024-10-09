8cb6663b623fc87b087d89d35176655b51258df5
Satyam Shivam <kumarshivam71552@gmail.com>
2024-10-09 11:31:45 +0000

    readme

---
+++ tut10.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+  
+
+      // ----------->> structures <<-----------
+
+
+struct employee
+{
+    int eId;
+    char favchar;
+    float salary;
+};
+
+int main()
+{
+    
+   
+    struct employee shivam;
+     shivam.eId=1;
+     shivam.salary=1000000;
+     shivam.favchar= 'A';
+     cout<<"the eId of shivam.eId is : "<<shivam.eId<<endl;
+     cout<<"the favchar of shivam is :"<<shivam.favchar<<endl;
+     cout<<"the salary of shivam is :"<<shivam.salary<<endl;
+
+}
+
+
